1d66152cf8be3cb736824442019735178e0283dc
KisaPulga <leoschaeffer03@gmail.com>
2025-01-29 13:57:30 +0100

    Classe livre

---
+++ GestionBibliotheque/GestionBibliotheque/Livre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Livre
+{
+    private string Titre;
+    private string Auteur;
+    private bool EstDisponible = true;
+    private string NomEmprunteur;
+    private DateTime DateEmprunt;
+    private DateTime DateRetour;
+
+    public Livre(string titre, string auteur)
+    {
+        Titre = titre;
+        Auteur = auteur;
+    }
+
+    public override string ToString() => $"Le livre {Titre}, écrit par {Auteur} est {(EstDisponible ? "disponible" : $"emprunté par {NomEmprunteur}")}";
+}
---
+++ GestionBibliotheque/GestionBibliotheque/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GestionBibliotheque
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Livre l = new Livre("Titanic", "Saoul");
+            Console.WriteLine(l);
+        }
+    }
+}
